7e87c55e26668b8300aa3d458cceaf690a4479eb
Levi Marvin <levimarvin@icloud.com>
2023-01-19 15:45:58 +0800

    Import more soong namespaces.

--- device.mk
+++ device.mk
@@ -76,7 +76,10 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    vendor/qcom/opensource/usb/etc \
 
 # Update engine
 PRODUCT_PACKAGES += \
